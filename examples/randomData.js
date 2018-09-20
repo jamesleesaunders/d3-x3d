0e28f66b885053c278e886d050c6aaadca7433bd
James Saunders <james@saunders-family.net>
2018-09-20 23:12:13 +0100

    Create surface area test data.

--- examples/randomData.js
+++ examples/randomData.js
@@ -118,3 +118,50 @@ function randomDataset4() {
 
 	return data;
 }
+
+/**
+ * Random Dataset - Surface Area 2
+ *
+ * @returns {*}
+ */
+function randomDataset5() {
+	var data = [
+		[
+			{ x: 0, y: 4, z: 0 },
+			{ x: 0, y: 0, z: 1 },
+			{ x: 0, y: 2, z: 2 },
+			{ x: 0, y: 0, z: 3 },
+			{ x: 0, y: 0, z: 4 }
+		],
+		[
+			{ x: 1, y: 4, z: 0 },
+			{ x: 1, y: 0, z: 1 },
+			{ x: 1, y: 2, z: 2 },
+			{ x: 1, y: 0, z: 3 },
+			{ x: 1, y: 0, z: 4 }
+		],
+		[
+			{ x: 2, y: 1, z: 0 },
+			{ x: 2, y: 0, z: 1 },
+			{ x: 2, y: 1, z: 2 },
+			{ x: 2, y: 0, z: 3 },
+			{ x: 2, y: 0, z: 4 }
+		],
+		[
+			{ x: 3, y: 4, z: 0 },
+			{ x: 3, y: 0, z: 1 },
+			{ x: 3, y: 2, z: 2 },
+			{ x: 3, y: 0, z: 3 },
+			{ x: 3, y: 0, z: 4 }
+		],
+		[
+			{ x: 4, y: 4, z: 0 },
+			{ x: 4, y: 0, z: 1 },
+			{ x: 4, y: 2, z: 2 },
+			{ x: 4, y: 0, z: 3 },
+			{ x: 4, y: 0, z: 4 }
+		]
+	];
+
+	return data;
+}
