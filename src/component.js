f37ef1fb10b6e908742730671d99fa64f2b40398
Victor Glindås <glinkis@gmail.com>
2018-11-05 15:23:18 +0100

    Fimplify import/export in component.

--- src/component.js
+++ src/component.js
@@ -1,21 +1,21 @@
-import { default as componentAxis } from "./component/axis";
-import { default as componentAxisThreePlane } from "./component/axisThreePlane";
-import { default as componentBars } from "./component/bars";
-import { default as componentBarsMultiSeries } from "./component/barsMultiSeries";
-import { default as componentBubbles } from "./component/bubbles";
-import { default as componentBubblesMultiSeries } from "./component/bubblesMultiSeries";
-import { default as componentSurfaceArea } from "./component/surfaceArea";
-import { default as componentViewpoint } from "./component/viewpoint";
-import { default as componentRibbon } from "./component/ribbon";
+import axis from "./component/axis";
+import axisThreePlane from "./component/axisThreePlane";
+import bars from "./component/bars";
+import barsMultiSeries from "./component/barsMultiSeries";
+import bubbles from "./component/bubbles";
+import bubblesMultiSeries from "./component/bubblesMultiSeries";
+import surfaceArea from "./component/surfaceArea";
+import viewpoint from "./component/viewpoint";
+import ribbon from "./component/ribbon";
 
 export default {
-	axis: componentAxis,
-	axisThreePlane: componentAxisThreePlane,
-	bars: componentBars,
-	barsMultiSeries: componentBarsMultiSeries,
-	bubbles: componentBubbles,
-	bubblesMultiSeries: componentBubblesMultiSeries,
-	surfaceArea: componentSurfaceArea,
-	viewpoint: componentViewpoint,
-	ribbon: componentRibbon
+	axis,
+	axisThreePlane,
+	bars,
+	barsMultiSeries,
+	bubbles,
+	bubblesMultiSeries,
+	surfaceArea,
+	viewpoint,
+	ribbon
 };
