e83932833f71c24db590fe27dfc1bd2b8f043ca4
James Saunders <james@saunders-family.net>
2018-11-16 08:15:40 +0100

    Update src/chart/bubbleChart.js
    Co-Authored-By: Glinkis <glinkis@gmail.com>

--- src/chart/bubbleChart.js
+++ src/chart/bubbleChart.js
@@ -14,7 +14,7 @@ import component from "../component";
  * var myChart = d3.x3dom.chart.bubbleChart();
  * chartHolder.datum(myData).call(myChart);
  */
-export default function bubbleChart() {
+export default function() {
 
 	/* Default Properties */
 	let width = 500;
