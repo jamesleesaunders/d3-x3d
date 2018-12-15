693589e95dd8f9e82cf05c4317a300e9f50b49b3
James Saunders <james@saunders-family.net>
2018-12-15 21:18:34 +0000

    New ribbon chart and components.

---
+++ src/chart/ribbonChartMultiSeries.js
@@ -0,0 +1,244 @@
+import * as d3 from "d3";
+import dataTransform from "../dataTransform";
+import component from "../component";
+
+/**
+ * Reusable 3D Multi Series Ribbon Chart
+ *
+ * @module
+ *
+ * @see https://datavizproject.com/data-type/waterfall-plot/
+ * @example
+ * var chartHolder = d3.select("#chartholder");
+ * var myData = [...];
+ * var myChart = d3.x3dom.chart.ribbonChartMultiSeries();
+ * chartHolder.datum(myData).call(myChart);
+ */
+export default function() {
+
+	/* Default Properties */
+	let width = 500;
+	let height = 500;
+	let dimensions = { x: 60, y: 40, z: 40 };
+	let colors = ["green", "red", "yellow", "steelblue", "orange"];
+	let classed = "x3dRibbonChartMultiSeries";
+	let debug = false;
+
+	/* Scales */
+	let xScale;
+	let yScale;
+	let zScale;
+	let colorScale;
+
+	/**
+	 * Initialise Data and Scales
+	 *
+	 * @private
+	 * @param {Array} data - Chart data.
+	 */
+	function init(data) {
+		const { rowKeys, columnKeys, valueMax } = dataTransform(data).summary();
+		const valueExtent = [0, valueMax];
+		const { x: dimensionX, y: dimensionY, z: dimensionZ } = dimensions;
+
+		if (typeof xScale === "undefined") {
+			xScale = d3.scalePoint()
+				.domain(columnKeys)
+				.range([0, dimensionX]);
+		}
+
+		if (typeof yScale === "undefined") {
+			yScale = d3.scaleLinear()
+				.domain(valueExtent)
+				.range([0, dimensionY])
+				.nice();
+		}
+
+		if (typeof zScale === "undefined") {
+			zScale = d3.scaleBand()
+				.domain(rowKeys)
+				.range([0, dimensionZ])
+				.padding(0.4);
+		}
+
+		if (typeof colorScale === "undefined") {
+			colorScale = d3.scaleOrdinal()
+				.domain(columnKeys)
+				.range(colors);
+		}
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @constructor
+	 * @alias ribbonChartMultiSeries
+	 * @param {d3.selection} selection - The chart holder D3 selection.
+	 */
+	function my(selection) {
+		const x3d = selection.append("x3d")
+			.attr("width", width + "px")
+			.attr("height", height + "px");
+
+		if (debug) {
+			x3d.attr("showLog", "true").attr("showStat", "true")
+		}
+
+		const scene = x3d.append("scene");
+
+		// Update the chart dimensions and add layer groups
+		const layers = ["axis", "chart"];
+		scene.classed(classed, true)
+			.selectAll("group")
+			.data(layers)
+			.enter()
+			.append("group")
+			.attr("class", (d) => d);
+
+		const viewpoint = component.viewpoint()
+			.centerOfRotation([dimensions.x / 2, dimensions.y / 2, dimensions.z / 2])
+			.viewOrientation([-0.61021, 0.77568, 0.16115, 0.65629])
+			.viewPosition([77.63865, 54.69470, 104.38314]);
+		scene.call(viewpoint);
+
+		scene.append("directionallight")
+			.attr("direction", "1 0 -1")
+			.attr("on", "true")
+			.attr("intensity", "0.4")
+			.attr("shadowintensity", "0");
+
+		scene.each((data) => {
+			init(data);
+
+			// Construct Axis Component
+			const axis = component.axisThreePlane()
+				.xScale(xScale)
+				.yScale(yScale)
+				.zScale(zScale);
+
+			// Construct Bars Component
+			const chart = component.ribbonMultiSeries()
+				.xScale(xScale)
+				.yScale(yScale)
+				.zScale(zScale)
+				.colors(colors);
+
+			scene.select(".axis")
+				.call(axis);
+
+			scene.select(".chart")
+				.datum(data)
+				.call(chart);
+		});
+	}
+
+	/**
+	 * Width Getter / Setter
+	 *
+	 * @param {number} _v - X3D canvas width in px.
+	 * @returns {*}
+	 */
+	my.width = function(_v) {
+		if (!arguments.length) return width;
+		width = _v;
+		return this;
+	};
+
+	/**
+	 * Height Getter / Setter
+	 *
+	 * @param {number} _v - X3D canvas height in px.
+	 * @returns {*}
+	 */
+	my.height = function(_v) {
+		if (!arguments.length) return height;
+		height = _v;
+		return this;
+	};
+
+	/**
+	 * Dimensions Getter / Setter
+	 *
+	 * @param {{x: number, y: number, z: number}} _v - 3D object dimensions.
+	 * @returns {*}
+	 */
+	my.dimensions = function(_v) {
+		if (!arguments.length) return dimensions;
+		dimensions = _v;
+		return this;
+	};
+
+	/**
+	 * X Scale Getter / Setter
+	 *
+	 * @param {d3.scale} _v - D3 scale.
+	 * @returns {*}
+	 */
+	my.xScale = function(_v) {
+		if (!arguments.length) return xScale;
+		xScale = _v;
+		return my;
+	};
+
+	/**
+	 * Y Scale Getter / Setter
+	 *
+	 * @param {d3.scale} _v - D3 scale.
+	 * @returns {*}
+	 */
+	my.yScale = function(_v) {
+		if (!arguments.length) return yScale;
+		yScale = _v;
+		return my;
+	};
+
+	/**
+	 * Z Scale Getter / Setter
+	 *
+	 * @param {d3.scale} _v - D3 scale.
+	 * @returns {*}
+	 */
+	my.zScale = function(_v) {
+		if (!arguments.length) return zScale;
+		zScale = _v;
+		return my;
+	};
+
+	/**
+	 * Color Scale Getter / Setter
+	 *
+	 * @param {d3.scale} _v - D3 color scale.
+	 * @returns {*}
+	 */
+	my.colorScale = function(_v) {
+		if (!arguments.length) return colorScale;
+		colorScale = _v;
+		return my;
+	};
+
+	/**
+	 * Colors Getter / Setter
+	 *
+	 * @param {Array} _v - Array of colours used by color scale.
+	 * @returns {*}
+	 */
+	my.colors = function(_v) {
+		if (!arguments.length) return colors;
+		colors = _v;
+		return my;
+	};
+
+	/**
+	 * Debug Getter / Setter
+	 *
+	 * @param {boolean} _v - Show debug log and stats. True/False.
+	 * @returns {*}
+	 */
+	my.debug = function(_v) {
+		if (!arguments.length) return debug;
+		debug = _v;
+		return my;
+	};
+
+	return my;
+}
