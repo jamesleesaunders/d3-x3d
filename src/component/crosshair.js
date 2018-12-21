ee3c07bfb120623336f9f880cbf48db3caecd839
James Saunders <james@saunders-family.net>
2018-12-21 22:20:37 +0000

    Improvement to Crosshair enter, update pattern.

--- src/component/crosshair.js
+++ src/component/crosshair.js
@@ -1,15 +1,6 @@
 // import * as d3 from "d3";
 
 /**
- * Random Number Generator between 1 and 10
- *
- * @returns {number}
- */
-function randomNum() {
-	return Math.floor(Math.random() * 10) + 1;
-}
-
-/**
  * Reusable 3D Crosshair Component
  *
  * @module
@@ -28,34 +19,6 @@ function crosshair() {
 	let zScale;
 
 	/**
-	 * Initialise Data and Scales
-	 *
-	 * @private
-	 */
-	function init() {
-		const { x: dimensionX, y: dimensionY, z: dimensionZ } = dimensions;
-		const extent = [0, 10];
-
-		if (typeof xScale === "undefined") {
-			xScale = d3.scaleLinear()
-				.domain(extent)
-				.rangeRound([0, dimensionX]);
-		}
-
-		if (typeof yScale === "undefined") {
-			yScale = d3.scaleLinear()
-				.domain(extent)
-				.range([0, dimensionY]);
-		}
-
-		if (typeof zScale === "undefined") {
-			zScale = d3.scaleLinear()
-				.domain(extent)
-				.range([0, dimensionZ]);
-		}
-	}
-
-	/**
 	 * Constructor
 	 *
 	 * @constructor
@@ -63,57 +26,63 @@ function crosshair() {
 	 * @param {d3.selection} selection - The chart holder D3 selection.
 	 */
 	function my(selection) {
-		init();
 		selection.classed(classed, true);
 
-		let data = [
-			{ color: "blue", direction: "x" },
-			{ color: "red", direction: "y" },
-			{ color: "green", direction: "z" }
-		];
+		selection.each((data) => {
 
-		let xVal = xScale(randomNum());
-		let yVal = yScale(randomNum());
-		let zVal = zScale(randomNum());
+			const xOff = dimensions["x"] / 2;
+			const yOff = dimensions["y"] / 2;
+			const zOff = dimensions["z"] / 2;
+			const xVal = xScale(data.x);
+			const yVal = yScale(data.y);
+			const zVal = zScale(data.z);
 
-		const axisDirectionVectors = {
-			x: [20, yVal, zVal],
-			y: [xVal, 20, zVal],
-			z: [xVal, yVal, 20]
-		};
+			const positionVectors = {
+				x: [xOff, yVal, zVal],
+				y: [xVal, yOff, zVal],
+				z: [xVal, yVal, zOff]
+			};
 
-		const axisRotationVectors = {
-			x: [1, 1, 0, Math.PI],
-			y: [0, 0, 0, 0],
-			z: [0, 1, 1, Math.PI]
-		};
-
-		function getAxisDirectionVector(axisDir) {
-			return axisDirectionVectors[axisDir];
-		}
-
-		function getAxisRotationVector(axisDir) {
-			return axisRotationVectors[axisDir];
-		}
-
-		data.forEach((d) => {
-				let shape = selection
-					.append("transform")
-					.attr("translation", getAxisDirectionVector(d.direction).join(" "))
-					.attr("rotation", getAxisRotationVector(d.direction).join(" "))
-					.append("shape")
-					.attr("class", "tickLine");
-
-				shape.append("cylinder")
-					.attr("radius", radius)
-					.attr("height", dimensions[d.direction]);
-
-				shape
-					.append("appearance")
-					.append("material")
-					.attr("diffuseColor", d.color);
+			function getPositionVector(axisDir) {
+				return positionVectors[axisDir].join(" ");
 			}
-		);
+
+			const rotationVectors = {
+				x: [1, 1, 0, Math.PI],
+				y: [0, 0, 0, 0],
+				z: [0, 1, 1, Math.PI]
+			};
+
+			function getRotationVector(axisDir) {
+				return rotationVectors[axisDir].join(" ");
+			}
+
+			const colorScale = d3.scaleOrdinal()
+				.domain(Object.keys(dimensions))
+				.range(colors);
+
+			const lineSelect = selection.selectAll(".line")
+				.data(Object.keys(dimensions));
+
+			const line = lineSelect.enter()
+				.append("transform")
+				.classed("line", true)
+				.attr("translation", (d) => getPositionVector(d))
+				.attr("rotation", (d) => getRotationVector(d))
+				.append("shape");
+
+			line.append("cylinder")
+				.attr("radius", radius)
+				.attr("height", (d) => dimensions[d]);
+
+			line.append("appearance")
+				.append("material")
+				.attr("diffuseColor", (d) => colorScale(d));
+
+			lineSelect.transition()
+				.attr("translation", (d) => getPositionVector(d))
+				.attr("rotation", (d) => getRotationVector(d));
+		});
 	}
 
 	/**
