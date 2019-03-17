765e7c7a0e46560cf02ad218b8853bb668b40d6a
James Saunders <james@saunders-family.net>
2019-03-17 21:07:32 +0000

    Run Mocha & Tape tests together.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,36 +1,36 @@
 let chai = require('chai');
-let expect = chai.expect;
+let mocha = require('mocha');
 let window = require('browser-env')();
 let d3 = require('d3');
 let d3X3dom = require('../');
 
-describe('Test Index', function () {
-  let author = 'James Saunders';
-  let license = 'GPL-2.0';
-  let year = new Date().getFullYear();
+mocha.describe('Test Index', function() {
+	let author = 'James Saunders';
+	let license = 'GPL-2.0';
+	let year = new Date().getFullYear();
 
-  it('Returns author', function (done) {
-    expect(d3X3dom.author).to.equal(author);
-    done();
-  })
-
-  it('Returns license', function (done) {
-    expect(d3X3dom.license).to.equal(license);
-    done();
-  })
-
-  it('Returns copyright', function (done) {
-    expect(d3X3dom.copyright).to.equal(`Copyright (C) ${year} ${author}`);
-    done();
-	  });
-
-  it('Returns chart object', function (done) {
-    expect(d3X3dom.chart).to.be.an('object');
-    done();
+	mocha.it('Returns author', function(done) {
+		chai.expect(d3X3dom.author).to.equal(author);
+		done();
 	});
 
-  it('Returns component object', function (done) {
-    expect(d3X3dom.component).to.be.an('object');
-    done();
+	mocha.it('Returns license', function(done) {
+		chai.expect(d3X3dom.license).to.equal(license);
+		done();
+	});
+
+	mocha.it('Returns copyright', function(done) {
+		chai.expect(d3X3dom.copyright).to.equal(`Copyright (C) ${year} ${author}`);
+		done();
+	});
+
+	mocha.it('Returns chart object', function(done) {
+		chai.expect(d3X3dom.chart).to.be.an('object');
+		done();
+	});
+
+	mocha.it('Returns component object', function(done) {
+		chai.expect(d3X3dom.component).to.be.an('object');
+		done();
 	});
 });
--- test/randomDataTest.js
+++ test/randomDataTest.js
@@ -1,40 +1,40 @@
 let chai = require('chai');
-let expect = chai.expect;
+let mocha = require('mocha');
 let window = require('browser-env')();
 let d3 = require('d3');
 let d3X3dom = require('../');
 
-describe('Test Random Data Generation', () => {
-  let randomData = d3X3dom.randomData;
-  let countries = ['UK', 'France', 'Spain', 'Germany', 'Italy', 'Portugal'];
-  let fruit = ['Apples', 'Oranges', 'Pears', 'Bananas'];
-  let data = randomData.dataset1();
-  let keys = Object.keys(data);
-  let key = Object.keys(data.key);
-  let values = Object.keys(data.values);
+mocha.describe('Test Random Data Generation', () => {
+	let randomData = d3X3dom.randomData;
+	let countries = ['UK', 'France', 'Spain', 'Germany', 'Italy', 'Portugal'];
+	let fruit = ['Apples', 'Oranges', 'Pears', 'Bananas'];
+	let data = randomData.dataset1();
+	let keys = Object.keys(data);
+	let key = Object.keys(data.key);
+	let values = Object.keys(data.values);
 
-  it('shoud return stores', (done) => {
-    expect(randomData.countries).to.be.deep.equal(countries);
-    done();
-  });
+	mocha.it('shoud return stores', (done) => {
+		chai.expect(randomData.countries).to.be.deep.equal(countries);
+		done();
+	});
 
-  it('shoud return fruits', (done) => {
-    expect(randomData.fruit).to.be.deep.equal(fruit);
-    done();
-  });
+	mocha.it('shoud return fruits', (done) => {
+		chai.expect(randomData.fruit).to.be.deep.equal(fruit);
+		done();
+	});
 
-  it('shoud return random Dataset 1 Keys', (done) => {
-    expect(keys).to.be.deep.equal(['key', 'values']);
-    done();
-  });
+	mocha.it('shoud return random Dataset 1 Keys', (done) => {
+		chai.expect(keys).to.be.deep.equal(['key', 'values']);
+		done();
+	});
 
-  it('shoud return random Dataset 1 Keys', (done) => {
-    expect(key).to.be.deep.equal(['0', '1', '2', '3', '4']);
-    done();
-  });
+	mocha.it('shoud return random Dataset 1 Keys', (done) => {
+		chai.expect(key).to.be.deep.equal(['0', '1', '2', '3', '4']);
+		done();
+	});
 
-  it('should return random Dataset 1 values', (done) => {
-    expect(values).to.be.deep.equal(['0', '1', '2', '3']);
-    done();
-  });
+	mocha.it('should return random Dataset 1 values', (done) => {
+		chai.expect(values).to.be.deep.equal(['0', '1', '2', '3']);
+		done();
+	});
 });
