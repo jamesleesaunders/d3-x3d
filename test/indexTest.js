b6cfd27c7d51e02b8adf180de1eb2fe2ecb24022
James Saunders <james@saunders-family.net>
2019-04-19 21:49:50 +0100

    Small tidy-ups.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,35 +1,35 @@
+let test = require('mocha');
 let chai = require('chai');
-let mocha = require('mocha');
 let window = require('browser-env')();
 let d3 = require('d3');
 let d3X3dom = require('../');
 
-mocha.describe('Test Index', function() {
+test.describe('Test Index', function() {
 	let author = 'James Saunders';
 	let license = 'GPL-2.0';
 	let year = new Date().getFullYear();
 
-	mocha.it('Returns author', function(done) {
+	test.it('Returns author', function(done) {
 		chai.expect(d3X3dom.author).to.equal(author);
 		done();
 	});
 
-	mocha.it('Returns license', function(done) {
+	test.it('Returns license', function(done) {
 		chai.expect(d3X3dom.license).to.equal(license);
 		done();
 	});
 
-	mocha.it('Returns copyright', function(done) {
+	test.it('Returns copyright', function(done) {
 		chai.expect(d3X3dom.copyright).to.equal(`Copyright (C) ${year} ${author}`);
 		done();
 	});
 
-	mocha.it('Returns chart object', function(done) {
+	test.it('Returns chart object', function(done) {
 		chai.expect(d3X3dom.chart).to.be.an('object');
 		done();
 	});
 
-	mocha.it('Returns component object', function(done) {
+	test.it('Returns component object', function(done) {
 		chai.expect(d3X3dom.component).to.be.an('object');
 		done();
 	});
--- test/randomDataTest.js
+++ test/randomDataTest.js
@@ -1,10 +1,10 @@
+let test = require('mocha');
 let chai = require('chai');
-let mocha = require('mocha');
 let window = require('browser-env')();
 let d3 = require('d3');
 let d3X3dom = require('../');
 
-mocha.describe('Test Random Data Generation', () => {
+test.describe('Test Random Data Generation', () => {
 	let randomData = d3X3dom.randomData;
 	let countries = ['UK', 'France', 'Spain', 'Germany', 'Italy', 'Portugal'];
 	let fruit = ['Apples', 'Oranges', 'Pears', 'Bananas'];
@@ -13,27 +13,27 @@ mocha.describe('Test Random Data Generation', () => {
 	let key = Object.keys(data.key);
 	let values = Object.keys(data.values);
 
-	mocha.it('shoud return stores', (done) => {
+	test.it('shoud return stores', (done) => {
 		chai.expect(randomData.countries).to.be.deep.equal(countries);
 		done();
 	});
 
-	mocha.it('shoud return fruits', (done) => {
+	test.it('shoud return fruits', (done) => {
 		chai.expect(randomData.fruit).to.be.deep.equal(fruit);
 		done();
 	});
 
-	mocha.it('shoud return random Dataset 1 Keys', (done) => {
+	test.it('shoud return random Dataset 1 Keys', (done) => {
 		chai.expect(keys).to.be.deep.equal(['key', 'values']);
 		done();
 	});
 
-	mocha.it('shoud return random Dataset 1 Keys', (done) => {
+	test.it('shoud return random Dataset 1 Keys', (done) => {
 		chai.expect(key).to.be.deep.equal(['0', '1', '2', '3', '4']);
 		done();
 	});
 
-	mocha.it('should return random Dataset 1 values', (done) => {
+	test.it('should return random Dataset 1 values', (done) => {
 		chai.expect(values).to.be.deep.equal(['0', '1', '2', '3']);
 		done();
 	});
