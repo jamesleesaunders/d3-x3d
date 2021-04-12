0ca74bb0bcd95e575eef96a576f40c85f2c08191
James Saunders <james@saunders-family.net>
2021-04-12 11:21:54 +0100

    Update d3-x3d.js

--- dist/d3-x3d.js
+++ dist/d3-x3d.js
@@ -2,7 +2,7 @@
  * d3-x3d
  *
  * @author James Saunders [james@saunders-family.net]
- * @copyright Copyright (C) 2020 James Saunders
+ * @copyright Copyright (C) 2021 James Saunders
  * @license GPLv2
  */
 
