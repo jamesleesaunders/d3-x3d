d17ebb975689bb780ea79a4aa200df59bea90c85
James Saunders <james@saunders-family.net>
2023-01-05 22:27:52 +0000

    Update browser-env.

--- test/randomDataTest.js
+++ test/randomDataTest.js
@@ -12,22 +12,22 @@ test.describe("Test Random Data Generation", () => {
 	let key = Object.keys(data.key);
 	let values = Object.keys(data.values);
 
-	test.it("shoud return stores", (done) => {
+	test.it("should return stores", (done) => {
 		chai.expect(randomData.countries).to.be.deep.equal(countries);
 		done();
 	});
 
-	test.it("shoud return fruits", (done) => {
+	test.it("should return fruits", (done) => {
 		chai.expect(randomData.fruit).to.be.deep.equal(fruit);
 		done();
 	});
 
-	test.it("shoud return random Dataset 1 Keys", (done) => {
+	test.it("should return random Dataset 1 Keys", (done) => {
 		chai.expect(keys).to.be.deep.equal(["key", "values"]);
 		done();
 	});
 
-	test.it("shoud return random Dataset 1 Keys", (done) => {
+	test.it("should return random Dataset 1 Keys", (done) => {
 		chai.expect(key).to.be.deep.equal(["0", "1", "2", "3", "4"]);
 		done();
 	});
